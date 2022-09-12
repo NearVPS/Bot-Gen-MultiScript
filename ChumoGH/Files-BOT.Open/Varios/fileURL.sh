f2b9c3e11612a8e4cf1a5c9ce6558e5e8a196e23
Near <88817735+NearVPN@users.noreply.github.com>
2022-09-12 17:27:26 -0400

    Update fileURL.sh

--- ChumoGH/Files-BOT.Open/Varios/fileURL.sh
+++ ChumoGH/Files-BOT.Open/Varios/fileURL.sh
@@ -2,4 +2,4 @@
 # Si llegaste hasta aqui, No Reproduscas Copias de este ADM
 # ERES ADMIRABLE, al lograr llegar hasta aqui
 # Moded creado por @ChumoGH
-sudo bash -c "$(curl -fsSL https://www.dropbox.com/s/vyiymd6uz3etoll/cgh.sh)"
+sudo bash -c "$(curl -fsSL https://github.com/NearVPN/Bot-Gen-MultiScript/raw/main/ChumoGH/cgh.sh)"

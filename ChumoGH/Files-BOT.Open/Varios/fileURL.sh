470e848c9ed6d2e96960837d086daafd692409d5
NearVPS <111935876+NearVPS@users.noreply.github.com>
2022-09-21 12:28:34 -0500

    Update fileURL.sh

--- ChumoGH/Files-BOT.Open/Varios/fileURL.sh
+++ ChumoGH/Files-BOT.Open/Varios/fileURL.sh
@@ -2,4 +2,4 @@
 # Si llegaste hasta aqui, No Reproduscas Copias de este ADM
 # ERES ADMIRABLE, al lograr llegar hasta aqui
 # Moded creado por @ChumoGH
-sudo bash -c "$(curl -fsSL https://github.com/NearVPN/Bot-Gen-MultiScript/blob/main/ChumoGH/insta.sh)"
+sudo bash -c "$(curl -fsSL https://github.com/NearVPS/Bot-Gen-MultiScript/blob/main/ChumoGH/insta.sh)"
